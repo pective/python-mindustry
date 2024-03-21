9abf4ab318dceb2be51321c60a61583d86e9e71c
pective <139483386+pective@users.noreply.github.com>
2024-03-21 12:07:18 +0100

    Made the initial pygame boilerplate

--- mindustry.py
+++ mindustry.py
@@ -1,4 +1,19 @@
 import pygame
 import random
 
+pygame.init()
+pygame.display.set_caption("Mindustry Python")
+surface = pygame.display.set_mode((800, 600))
+delta = 0
+fps = 60
 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    surface.fill('black')
+    pygame.display.flip()
+    delta = clock.tick(fps)
+pygame.quit
